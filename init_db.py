49c91db465f8d3cceeec2f18a0ba8c1083af505a
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-09-02 09:48:09 +0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,10 +1,4 @@
-from app import app, db
-
-def init_db():
-    with app.app_context():
-        db.create_all()
-        print("Database tables created.")
-
 if __name__ == '__main__':
-    init_db()
-
+    with app.app_context():
+        create_tables()  # Создание таблиц перед запуском приложения
+    app.run(debug=True)
